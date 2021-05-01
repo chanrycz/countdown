a0261426b04ab322172d3d937a6feb648c6290b1
chanrycz <65661524+chanrycz@users.noreply.github.com>
2021-05-01 22:46:00 +0800

    v1.1.8

--- js/start_test.js
+++ js/start_test.js
@@ -1,1 +1,140 @@
-const xhttp=new XMLHttpRequest;xhttp.overrideMimeType("application/json"),xhttp.onreadystatechange=function(){if(4==xhttp.readyState&&"200"==xhttp.status){const s=JSON.parse(xhttp.responseText);var a,b,c,d,e=new Date(s.countDate).getTime(),f=s.mainEndMessage,g=s.snowingMode,h=s.endFireworks;if(!0===g&&setInterval(createSnowFlake,150),0!==s.otherEvents.length)for(var j in a=s.otherEventsTitle,b=s.otherEvents,b)b[j].hasOwnProperty("date")?b[j].hasOwnProperty("separator")?(c=b[j].date,d=new Date(c).toLocaleDateString("en-US",{month:"short",day:"numeric"}),a+="<br>"+b[j].name+b[j].separator+d):(c=b[j].date,d=new Date(c).toLocaleDateString("en-US",{month:"short",day:"numeric"}),a+="<br>"+b[j].name+" &mdash; "+d):a+=b[j].hasOwnProperty("separator")?"<br>"+b[j].name+b[j].separator+b[j].text:"<br>"+b[j].name+" &mdash; "+b[j].text;else a="",b="";var i=new Date().getTime(),k=e-i,l=Math.floor(k/86400000),m=Math.floor(k%86400000/3600000),n=Math.floor(k%3600000/60000),o=Math.floor(k%60000/1e3);if(document.getElementById("day").innerHTML=100<=l?("0"+l).slice(-3):("0"+l).slice(-2),document.getElementById("hour").innerHTML=("0"+m).slice(-2),document.getElementById("minute").innerHTML=("0"+n).slice(-2),document.getElementById("second").innerHTML=("0"+o).slice(-2),0>k&&(clearInterval(r),document.getElementById("day").innerHTML="00",document.getElementById("hour").innerHTML="00",document.getElementById("minute").innerHTML="00",document.getElementById("second").innerHTML="00",document.getElementById("day").classList.add("blinking"),document.getElementById("hour").classList.add("blinking"),document.getElementById("minute").classList.add("blinking"),document.getElementById("second").classList.add("blinking"),document.getElementById("endtext").innerHTML=f,!0===h)){var p=document.createElement("link");p.setAttribute("rel","stylesheet"),p.setAttribute("href","css/fireworks.css"),document.getElementsByTagName("head")[0].appendChild(p);var q=document.createElement("div");q.classList.add("pyro"),document.body.appendChild(q)}var r=setInterval(function(){var a=new Date().getTime(),b=e-a,c=Math.floor(b/86400000),d=Math.floor(b%86400000/3600000),g=Math.floor(b%3600000/60000),i=Math.floor(b%60000/1e3);if(document.getElementById("day").innerHTML=100<=c?("0"+c).slice(-3):("0"+c).slice(-2),document.getElementById("hour").innerHTML=("0"+d).slice(-2),document.getElementById("minute").innerHTML=("0"+g).slice(-2),document.getElementById("second").innerHTML=("0"+i).slice(-2),0>b&&(clearInterval(r),document.getElementById("day").innerHTML="00",document.getElementById("hour").innerHTML="00",document.getElementById("minute").innerHTML="00",document.getElementById("second").innerHTML="00",document.getElementById("day").classList.add("blinking"),document.getElementById("hour").classList.add("blinking"),document.getElementById("minute").classList.add("blinking"),document.getElementById("second").classList.add("blinking"),document.getElementById("endtext").innerHTML=f,!0===h)){var j=document.createElement("link");j.setAttribute("rel","stylesheet"),j.setAttribute("href","css/fireworks.css"),document.getElementsByTagName("head")[0].appendChild(j);var k=document.createElement("div");k.classList.add("pyro"),document.body.appendChild(k)}},500);document.getElementById("title").innerHTML=s.mainEvent,document.getElementById("endtext").innerHTML=a}},xhttp.open("GET","settings/events_test.json?_="+new Date().getTime(),!0),xhttp.send();function createSnowFlake(){const a=document.createElement("i");a.classList.add("fas"),a.classList.add("fa-snowflake"),a.style.left=Math.random()*window.innerWidth+"px",a.style.animationDuration=3*Math.random()+2+"s",a.style.opacity=Math.random(),a.style.fontSize=10*Math.random()+10+"px",document.body.appendChild(a),setTimeout(()=>{a.remove()},5e3)}
+// Create XMLHttpRequest to fetch JSON data
+const xhttp = new XMLHttpRequest();
+xhttp.overrideMimeType("application/json");
+xhttp.onreadystatechange = function () {
+	if (xhttp.readyState == 4 && xhttp.status == "200") {
+		const data = JSON.parse(xhttp.responseText);
+		var eventMessage;
+		var events_data;
+		var dateParse;
+		var dateFormatted;
+        var deadline = new Date( data.countDate ).getTime();
+        var endmessage = data.mainEndMessage;
+        var snowing = data.snowingMode;
+        var fireworks = data.endFireworks;
+        if(snowing === true) {
+            setInterval(createSnowFlake, 150);
+        }
+        if(data.otherEvents.length !== 0) {
+            eventMessage = data.otherEventsTitle;
+            events_data = data.otherEvents;
+            for (var i in events_data) {
+                if (events_data[i].hasOwnProperty("date")) {
+                    if (events_data[i].hasOwnProperty("separator")) {
+                        dateParse = events_data[i].date;
+                        dateFormatted = new Date(dateParse).toLocaleDateString("en-US", {month: 'short', day: 'numeric'});
+                        eventMessage += "<br>" + events_data[i].name + events_data[i].separator + dateFormatted;
+                    } else {
+                        dateParse = events_data[i].date;
+                        dateFormatted = new Date(dateParse).toLocaleDateString("en-US", {month: 'short', day: 'numeric'});
+                        eventMessage += "<br>" + events_data[i].name + " &mdash; " + dateFormatted;
+                    }
+                }
+                else {
+                    if (events_data[i].hasOwnProperty("separator")) {
+                        eventMessage += "<br>" + events_data[i].name + events_data[i].separator + events_data[i].text;
+                    } else {
+                        eventMessage += "<br>" + events_data[i].name + " &mdash; " + events_data[i].text;
+                    }
+                }
+            }
+        } else {
+            eventMessage = data.otherEventsTitle;
+            events_data = "";
+        }
+        
+        var now = new Date().getTime(); 
+        var t = deadline - now; 
+        var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+        var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+        var seconds = Math.floor((t % (1000 * 60)) / 1000);
+        if (days >= 100) {
+            document.getElementById("day").innerHTML = ("0" + days).slice(-3);
+        } else {
+            document.getElementById("day").innerHTML = ("0" + days).slice(-2);
+        }
+        document.getElementById("hour").innerHTML = ("0" + hours).slice(-2);
+        document.getElementById("minute").innerHTML = ("0" + minutes).slice(-2);
+        document.getElementById("second").innerHTML = ("0" + seconds).slice(-2);
+        if (t < 0) { 
+             clearInterval(x);
+             document.getElementById("day").innerHTML = "00"; 
+             document.getElementById("hour").innerHTML = "00"; 
+             document.getElementById("minute").innerHTML = "00" ;
+             document.getElementById("second").innerHTML = "00"; 
+             document.getElementById("day").classList.add("blinking"); 
+             document.getElementById("hour").classList.add("blinking"); 
+             document.getElementById("minute").classList.add("blinking");
+             document.getElementById("second").classList.add("blinking");
+             document.getElementById("endtext").innerHTML = endmessage;
+             if (fireworks === true) {
+                 var fireworkCSS = document.createElement("link");
+                 fireworkCSS.setAttribute("rel", "stylesheet");
+                 fireworkCSS.setAttribute("href", "css/fireworks.css");
+                 document.getElementsByTagName("head")[0].appendChild(fireworkCSS);
+                 var pyroDiv = document.createElement("div");
+                 pyroDiv.classList.add("pyro");
+                 document.body.appendChild(pyroDiv);
+             }
+        }
+        
+        
+        var x = setInterval(function() { 
+        var now = new Date().getTime(); 
+        var t = deadline - now; 
+        var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+        var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+        var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+        if (days >= 100) {
+            document.getElementById("day").innerHTML = ("0" + days).slice(-3);
+        } else {
+            document.getElementById("day").innerHTML = ("0" + days).slice(-2);
+        }
+        document.getElementById("hour").innerHTML = ("0" + hours).slice(-2);
+        document.getElementById("minute").innerHTML = ("0" + minutes).slice(-2);
+        document.getElementById("second").innerHTML = ("0" + seconds).slice(-2);
+        if (t < 0) { 
+             clearInterval(x);
+             document.getElementById("day").innerHTML = "00"; 
+             document.getElementById("hour").innerHTML = "00"; 
+             document.getElementById("minute").innerHTML = "00" ;
+             document.getElementById("second").innerHTML = "00";
+             document.getElementById("day").classList.add("blinking"); 
+             document.getElementById("hour").classList.add("blinking"); 
+             document.getElementById("minute").classList.add("blinking");
+             document.getElementById("second").classList.add("blinking");
+             document.getElementById("endtext").innerHTML = endmessage; 
+             if (fireworks === true) {
+                 var fireworkCSS = document.createElement("link");
+                 fireworkCSS.setAttribute("rel", "stylesheet");
+                 fireworkCSS.setAttribute("href", "css/fireworks.css");
+                 document.getElementsByTagName("head")[0].appendChild(fireworkCSS);
+                 var pyroDiv = document.createElement("div");
+                 pyroDiv.classList.add("pyro");
+                 document.body.appendChild(pyroDiv);
+             } } 
+        }, 500);
+        
+		document.getElementById("title").innerHTML = data.mainEvent;
+        document.getElementById("endtext").innerHTML = eventMessage;
+	}
+};
+xhttp.open("GET", "settings/events_test.json", true);
+xhttp.send();
+function createSnowFlake() {
+	const snow_flake = document.createElement('i');
+	snow_flake.classList.add('fas');
+	snow_flake.classList.add('fa-snowflake');
+	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+	snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's'; // between 2 - 4 seconds
+	snow_flake.style.opacity = Math.random();
+	snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
+	
+	document.body.appendChild(snow_flake);
+	
+	setTimeout(() => {
+		snow_flake.remove();
+	}, 5000)
+}
--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,60 @@
-var pattern=["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a"],patternRev=["a","b","ArrowRight","ArrowLeft","ArrowRight","ArrowLeft","ArrowDown","ArrowDown","ArrowUp","ArrowUp"],current=0,currentRev=0,patternOn=!1,patternRevOn=!1,keyHandler=function(a){return 0>pattern.indexOf(a.key)||a.key!==pattern[current]?void(current=0):void(current++,pattern.length===current&&!1===patternOn&&(current=0,patternOn=!0,document.getElementById("player").pause(),document.getElementById("player").src="audio/Gimkit.mp3",document.getElementById("play-pause").src="img/pause.png",document.getElementById("player").play(),playing=!0,songs.push("audio/Gimkit.mp3"),songIndex=songs.length-1,localStorage.setItem("schoolCountSongNum",songIndex),localStorage.setItem("gkEaster",!0),!1===patternRevOn&&document.addEventListener("keydown",keyHandlerRev,!1)))},keyHandlerRev=function(a){return 0>patternRev.indexOf(a.key)||a.key!==patternRev[currentRev]?void(currentRev=0):void(currentRev++,patternRev.length===currentRev&&!1===patternRevOn&&(currentRev=0,patternRevOn=!0,document.getElementById("player").pause(),document.getElementById("player").src="audio/Kahoot.mp3",document.getElementById("play-pause").src="img/pause.png",document.getElementById("player").play(),playing=!0,songs.push("audio/Kahoot.mp3"),songIndex=songs.length-1,localStorage.setItem("schoolCountSongNum",songIndex),localStorage.setItem("ktEaster",!0),!1===patternOn&&document.addEventListener("keydown",keyHandler,!1)))};"true"!=localStorage.getItem("gkEaster")&&document.addEventListener("keydown",keyHandler,!1),"true"==localStorage.getItem("ktEaster")&&document.addEventListener("keydown",keyHandlerRev,!1);
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var patternRev = ['a', 'b', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowDown', 'ArrowUp', 'ArrowUp'];
+var current = 0;
+var currentRev = 0;
+var patternOn = false;
+var patternRevOn = false;
+
+var keyHandler = function (event) {
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+	current++;
+	if (pattern.length === current && patternOn === false) {
+		current = 0;
+		patternOn = true;
+		document.getElementById("player").pause();
+		document.getElementById("player").src = "audio/Gimkit.mp3";
+		document.getElementById("play-pause").src = "img/pause.png";
+		document.getElementById("player").play();
+		playing = true;
+		songs.push("audio/Gimkit.mp3");
+		songIndex = (songs.length - 1);
+		localStorage.setItem('schoolCountSongNum', songIndex);
+		localStorage.setItem('gkEaster', true);
+		if (patternRevOn === false) {
+		    document.addEventListener('keydown', keyHandlerRev, false);
+		}
+	}
+};
+
+var keyHandlerRev = function (event) {
+	if (patternRev.indexOf(event.key) < 0 || event.key !== patternRev[currentRev]) {
+		currentRev = 0;
+		return;
+	}
+	currentRev++;
+	if (patternRev.length === currentRev && patternRevOn === false) {
+		currentRev = 0;
+		patternRevOn = true;
+		document.getElementById("player").pause();
+		document.getElementById("player").src = "audio/Kahoot.mp3";
+		document.getElementById("play-pause").src = "img/pause.png";
+		document.getElementById("player").play();
+		playing = true;
+		songs.push("audio/Kahoot.mp3");
+		songIndex = (songs.length - 1);
+		localStorage.setItem('schoolCountSongNum', songIndex);
+		localStorage.setItem('ktEaster', true);
+		if (patternOn === false) {
+		    document.addEventListener('keydown', keyHandler, false);
+		}
+	}
+};
+if (localStorage.getItem('gkEaster') != 'true') {
+    document.addEventListener('keydown', keyHandler, false);
+}
+if (localStorage.getItem('ktEaster') == 'true') {
+    document.addEventListener('keydown', keyHandlerRev, false);
+}
